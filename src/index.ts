7d08c13a8e403eea5c265aa4bc8711e1fa2ef2e9
saltyaom <saltyaom@gmail.com>
2022-01-14 18:27:27 +0700

    :tada: feat: estimate latest id

--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,32 @@
 import { mkdirSync, existsSync, writeFileSync } from 'fs'
 
-import fetch from 'isomorphic-unfetch'
-
 import PQueue from 'p-queue'
+import fetch from 'isomorphic-unfetch'
+import parse from 'node-html-parser'
 
 // NHentai Rate limit
 const queue = new PQueue({ concurrency: 6 })
 
 // ? Get estimate latest nhentai id
 const getLatest = async (): Promise<number | Error> => {
-    return 100
+    const html = await fetch('https://nhentai.net')
+        .then((res) => res.text())
+        .then((res) => parse(res))
 
-    // const html = await fetch('https://nhentai.net')
-    //     .then((res) => res.text())
-    //     .then((res) => parse(res))
+    const firstCover = html.querySelector(
+        '#content > .index-container:nth-child(2) > .gallery > .cover'
+    )
 
-    // const firstCover = html.querySelector(
-    //     '#content > .index-container:nth-child(2) > .gallery > .cover'
-    // )
+    if (!firstCover) throw new Error("Couldn't find first cover")
 
-    // if (!firstCover) throw new Error("Couldn't find first cover")
+    const url = firstCover.getAttribute('href')!
 
-    // const url = firstCover.getAttribute('href')!
+    const id = url
+        .split('/')
+        .reverse()
+        .find((x) => x)
 
-    // const id = url
-    //     .split('/')
-    //     .reverse()
-    //     .find((x) => x)
-
-    // return id ? parseInt(id) : new Error("Couldn't find id")
+    return id ? parseInt(id) : new Error("Couldn't find id")
 }
 
 const getNhentai = async (id: number): Promise<string | Error> => {
@@ -70,7 +68,7 @@ const main = async () => {
             writeFileSync(`data/${i}.json`, hentai)
         })
 
-        queue.add(() => new Promise((resolve) => setTimeout(resolve, 575)))
+        queue.add(() => new Promise((resolve) => setTimeout(resolve, 700)))
     }
 
     const progress = setInterval(() => {
